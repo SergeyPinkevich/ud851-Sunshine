4e25ce430248a10dcc317adaa4feb7716a61349e
Sergey Pinkevich <sergeipinkevich163@gmail.com>
2017-11-18 12:57:31 +0300

    get text in second activity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,6 +13,6 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        String text = getIntent().getStringExtra(Intent.EXTRA_TEXT);
     }
 }
